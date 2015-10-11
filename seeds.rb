9bde6d4f76b4c20d0953bd129dc99d233eaf1a39
Cena Mayo <cenazoic@gmail.com>
2015-10-11 17:12:52 -0500

    added original Postgres seeds.rb for conversion to JSON

--- seeds.rb
+++ seeds.rb
@@ -1,10 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#  TODO: 11 October 2015 
+# Convert to JSON
+# -- complete brewery/beers list
+# -- additional info?
 
 #BREWERIES
 br1 = Brewery.create(name:'Surly Brewing Company', city:'Minneapolis', year:2006)
